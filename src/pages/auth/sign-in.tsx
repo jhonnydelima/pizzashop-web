3ffdba00b0dd2a00ef17d8906577fdb9a9fd5019
Jhonny Lima <jhonny.lima@funcionalcorp.com.br>
2024-02-23 01:39:09 -0300

    feat: add field validation to sign in page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,43 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const signInFormSchema = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInFormSchema>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+        actionButtonStyle: {
+          backgroundColor: 'green',
+        },
+      })
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -20,13 +53,13 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button type="submit" className="w-full">
+            <Button disabled={isSubmitting} type="submit" className="w-full">
               Acessar painel
             </Button>
           </form>
